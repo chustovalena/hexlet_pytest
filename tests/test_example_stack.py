741ec8f7b320011a02a8dbdaba88a582cdc2513f
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2024-11-01 02:02:13 +0300

    create test_example_stack.py

---
+++ tests/test_example_stack.py
@@ -0,0 +1,29 @@
+from hexlet_pytest.example_stack import stack1
+import pytest
+
+
+def test_stack():
+	stack = []
+	
+	stack.append('one')
+	stack.append('two')
+
+	assert stack.pop() == "two"
+	assert stack.pop() == 'one'
+
+
+def test_emptiness():
+	stack = []
+	assert not stack
+	stack.append('one')
+	assert bool(stack)
+
+	stack.pop()
+	assert not stack
+
+
+def test_pop_with_empty_stack():
+	stack = []
+
+	with pytest.raises(IndexError):
+		stack.pop()
