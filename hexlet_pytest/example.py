eeab9a0454c9a3ba597893d179d9ac0ef38cf2fd
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2024-11-01 23:32:24 +0300

    update hexlet_pytest/example.py

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,14 @@
 def reverse(string):
+    """Reverse string
+    >>> reverse('')
+    ''
+
+    >>> reverse('Awesome')
+    'emosewA'
+    """
+
     return string[::-1]
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
